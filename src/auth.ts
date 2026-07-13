3cc179ad11a02de16e92805886df77a84c3fef93
wjexstudio <wasin.jex@gmail.com>
2026-07-14 03:34:43 +0700

    fix: fallback to existing google and nextauth env variables

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,12 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID || process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET || process.env.GOOGLE_CLIENT_SECRET,
+    })
+  ],
   callbacks: {
     async signIn({ user }) {
       const allowedEmails = process.env.ADMIN_EMAILS?.split(',') || [];
@@ -12,6 +17,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return false; // Access denied
     },
   },
+  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/login',
   },
